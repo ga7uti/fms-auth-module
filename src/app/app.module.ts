4e10ccb29d67c4877946dff4072e2752b0d05b26
ga7uti <gauti783@gmail.com>
2020-05-03 20:54:15 +0530

    added register ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './_auth/login/login.component';
 import {FormsModule} from '@angular/forms';
+import {RegisterComponent} from './_auth/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
