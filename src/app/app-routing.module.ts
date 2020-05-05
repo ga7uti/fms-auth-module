0ff44eabde404b816b533109fce30f251e766b02
ga7uti <gauti783@gmail.com>
2020-05-05 19:40:42 +0530

    update ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './_auth/login/login.component';
 import {RegisterComponent} from './_auth/register/register.component';
+import {UserComponent} from './_auth/user/user.component';
+import {AdminComponent} from './_auth/admin/admin.component';
 
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path:'register', component: RegisterComponent}
+  {path:'register', component: RegisterComponent},
+  {path:'user', component: UserComponent},
+  {path:'admin', component: AdminComponent}
 ];
 
 @NgModule({
